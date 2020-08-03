5e5ff7a12dd9bde2c1f05c42217d8a3e950d4f0e
Katie Le <lethikatie@gmail.com>
2020-08-03 22:07:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,6 +71,6 @@ end
     end 
     
   def list_pets
-    "I have #{numberofdogs} dog(s), and #{numberofcats} cat(s)."
+    "I have #{numberofdogs}.count dog(s), and #{numberofcats}.count cat(s)."
   end 
 end 
